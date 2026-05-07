953e5a3c34af38b7d788244e1dcf91892eb369ff
Vishnu <vishnu.git25@gmail.com>
2026-05-07 14:37:30 +0530

    basic conditions

--- practice.py
+++ practice.py
@@ -1,6 +1,33 @@
 #print("hello world");
 
-name = input("enter your name:  ")
-age = input("enter your age: ")
-print("hello",name)
-print("you are", age, "years old");
+
+# name = input("enter your name:  ")
+# # age = input("enter your age: ")
+# print("hello",name)
+# print("you are", age, "years old");
+
+
+# name = input("enter your name:  ")
+# # age = input("enter your age: ")
+# age = int(input("enter age: "))
+# age + 10 ;
+# print("hello",name)
+# print("you are", age + 10, "years old");
+
+
+# condition
+
+# age = int(input("enter age: "))
+# if age>= 18:
+#     print("you are legal")
+# else:
+#     print("get yo illegal az outtta here")
+
+
+age = int(input("enter age:"))
+if age>= 60 :
+    print("old")
+elif age>= 18:
+        print("adult")
+else:
+     print("minor")
